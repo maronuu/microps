11c7360bb955f89764db998fccfccf27941fe03e
maronuu <oguri.ytr.2000@gmail.com>
2022-03-04 21:08:28 +0900

    step11

--- icmp.c
+++ icmp.c
@@ -97,30 +97,41 @@ void icmp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, s
 
     debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)), ip_addr_ntop(dst, addr2, sizeof(addr2)), len);
     icmp_dump(data, len);
+
+    // switch by type
+    switch (hdr->type) {
+        case ICMP_TYPE_ECHO:
+            icmp_output(ICMP_TYPE_ECHOREPLY, hdr->code, hdr->values, (uint8_t *)(hdr + 1), len - sizeof(*hdr),
+                        iface->unicast, src);
+            break;
+        default:
+            // do nothing
+            break;
+    }
 }
 
-// int icmp_output(uint8_t type, uint8_t code, uint32_t values, const uint8_t *data, size_t len, ip_addr_t src,
-//                 ip_addr_t dst) {
-//     uint8_t buf[ICMP_BUFSIZ];
-//     struct icmp_hdr *hdr;
-//     size_t msg_len;  // ICMP message length = header + data
-//     char addr1[IP_ADDR_STR_LEN];
-//     char addr2[IP_ADDR_STR_LEN];
+int icmp_output(uint8_t type, uint8_t code, uint32_t values, const uint8_t *data, size_t len, ip_addr_t src,
+                ip_addr_t dst) {
+    uint8_t buf[ICMP_BUFSIZ];
+    struct icmp_hdr *hdr;
+    size_t msg_len;  // ICMP message length = header + data
+    char addr1[IP_ADDR_STR_LEN];
+    char addr2[IP_ADDR_STR_LEN];
 
-//     hdr = (struct icmp_hdr *)buf;
-//     hdr->type = type;
-//     hdr->code = code;
-//     hdr->values = values;
-//     hdr->sum = 0;
-//     memcpy(hdr + 1, data, len);
-//     msg_len = sizeof(hdr) + len;
-//     hdr->sum = cksum16((uint16_t *)data, len, 0);
+    hdr = (struct icmp_hdr *)buf;
+    hdr->type = type;
+    hdr->code = code;
+    hdr->values = values;
+    hdr->sum = 0;
+    memcpy(hdr + 1, data, len);
+    msg_len = sizeof(*hdr) + len;
+    hdr->sum = cksum16((uint16_t *)hdr, msg_len, 0);
 
-//     debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)), ip_addr_ntop(dst, addr2, sizeof(addr2)),
-//            msg_len);
-//     icmp_dump((uint8_t *)hdr, msg_len);
-
-// }
+    debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)), ip_addr_ntop(dst, addr2, sizeof(addr2)),
+           msg_len);
+    icmp_dump((uint8_t *)hdr, msg_len);
+    return ip_output(IP_PROTOCOL_ICMP, buf, msg_len, src, dst);
+}
 
 int icmp_init(void) {
     if (ip_protocol_register(IP_PROTOCOL_ICMP, icmp_input) == -1) {
